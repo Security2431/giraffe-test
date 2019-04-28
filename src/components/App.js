b7756887a97fa0103e86e1377bdc69e1be7b9b10
Artem Shcherbakov <Artem2431@gmail.com>
2019-04-28 15:27:39 +0300

    Clean up project

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit src/App.js and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +15,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
